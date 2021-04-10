d478b1ab46f3591d36324105753378de0f63ceee
ky27611 <80205643+ky27611@users.noreply.github.com>
2021-04-10 17:20:00 +0900

    タイトル画面の追加
    タイトル画面（仮）の追加

---
+++ Assets/TitleDirector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleDirector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Stage0Scene");
+            Debug.Log("stage0");
+        }
+    }
+}
